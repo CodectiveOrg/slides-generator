c103b7f02c05f73e0657dae46b363e9f59147664
BijanProgrammer <bijaneisapour@gmail.com>
2025-02-19 03:31:23 +0330

    [ADD] home page

---
+++ src/app/css-color-scheme/page.tsx
@@ -0,0 +1,54 @@
+import { ReactElement } from "react";
+
+import SlidesMasterComponent from "@/components/slides-master/slides-master.component";
+import CodeComponent from "@/components/code/code.component";
+
+import CoverSlide from "@/slides/cover/cover.slide";
+import BasicSlide from "@/slides/basic/basic.slide";
+
+export default function Page(): ReactElement {
+  return (
+    <SlidesMasterComponent>
+      <CoverSlide title="Dark Mode" subtitle="فقط با یک خط کد!" />
+      <BasicSlide title="color-scheme">
+        <p>برای فعال‌سازی دارک مود، کافیه این کد رو به فایل HTML اضافه کنید:</p>
+        <CodeComponent
+          language="css"
+          code={`
+html {
+  color-scheme: dark;
+}
+        `}
+        />
+        <p>
+          این کد به مرورگر میگه برای تمام المان‌های صفحه از نسخه‌ی تیره استفاده
+          کنه.
+        </p>
+        <p>
+          یعنی Scrollbar و Button و Checkbox Input و Date Picker و Select و خیلی
+          از المان‌های دیگه.
+        </p>
+      </BasicSlide>
+      <BasicSlide title="دو مقدار همزمان">
+        <p>همچنین میشه از دو مقدار به طور همزمان استفاده کرد:</p>
+        <CodeComponent
+          language="css"
+          code={`
+html {
+  color-scheme: light dark;
+}
+        `}
+        />
+        <p>
+          اینجوری به مرورگر میگیم که سایت ما، هم از Light Mode و هم از Dark Mode
+          پشتیبانی میکنه.
+        </p>
+        <p>بنابراین با توجه به تنظیمات سیستم کاربر، تم سایت مشخص میشه.</p>
+        <p>
+          اگه کاربر اولویت خاصی نداشته باشه، اولین مقدار در نظر گرفته میشه که
+          اینجا برابر با <code>light</code> ئه.
+        </p>
+      </BasicSlide>
+    </SlidesMasterComponent>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,54 +1,29 @@
 import { ReactElement } from "react";
 
-import SlidesMasterComponent from "@/components/slides-master/slides-master.component";
-import CodeComponent from "@/components/code/code.component";
+import Link from "next/link";
 
-import CoverSlide from "@/slides/cover/cover.slide";
-import BasicSlide from "@/slides/basic/basic.slide";
+import { glob } from "glob";
 
-export default function Page(): ReactElement {
+import styles from "./page.module.css";
+
+export default async function Page(): Promise<ReactElement> {
+  const routeFullPaths = await glob("app/**/page.tsx", { cwd: "src" });
+
+  const routes = routeFullPaths
+    .map((x) => x.split(/[/\\]/))
+    .map((x) => x.slice(1, x.length - 1))
+    .map((x) => x.join("/"))
+    .filter(Boolean);
+
   return (
-    <SlidesMasterComponent>
-      <CoverSlide title="Dark Mode" subtitle="فقط با یک خط کد!" />
-      <BasicSlide title="color-scheme">
-        <p>برای فعال‌سازی دارک مود، کافیه این کد رو به فایل HTML اضافه کنید:</p>
-        <CodeComponent
-          language="css"
-          code={`
-html {
-  color-scheme: dark;
-}
-        `}
-        />
-        <p>
-          این کد به مرورگر میگه برای تمام المان‌های صفحه از نسخه‌ی تیره استفاده
-          کنه.
-        </p>
-        <p>
-          یعنی Scrollbar و Button و Checkbox Input و Date Picker و Select و خیلی
-          از المان‌های دیگه.
-        </p>
-      </BasicSlide>
-      <BasicSlide title="دو مقدار همزمان">
-        <p>همچنین میشه از دو مقدار به طور همزمان استفاده کرد:</p>
-        <CodeComponent
-          language="css"
-          code={`
-html {
-  color-scheme: light dark;
-}
-        `}
-        />
-        <p>
-          اینجوری به مرورگر میگیم که سایت ما، هم از Light Mode و هم از Dark Mode
-          پشتیبانی میکنه.
-        </p>
-        <p>بنابراین با توجه به تنظیمات سیستم کاربر، تم سایت مشخص میشه.</p>
-        <p>
-          اگه کاربر اولویت خاصی نداشته باشه، اولین مقدار در نظر گرفته میشه که
-          اینجا برابر با <code>light</code> ئه.
-        </p>
-      </BasicSlide>
-    </SlidesMasterComponent>
+    <div className={styles.page}>
+      <ul>
+        {routes.map((page) => (
+          <li key={page}>
+            <Link href={`/${page}`}>{page}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
